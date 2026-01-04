a13d2a3ea3d91643bc718eb3b1afbc4adc2deb9c
Kartik Madan <137984841+Calm-Ocean@users.noreply.github.com>
2026-01-04 16:05:20 +0530

    Update categories.ts

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,8 +1,9 @@
-export const ANIMAL_CATEGORIES = ['Dogs', 'Cats', 'Birds', 'Fish']; // Removed 'Other'
+export const ANIMAL_CATEGORIES = ['Dogs', 'Cats', 'Birds', 'Fish', 'Small Animals'];
 
 export const CATEGORY_IMAGES: { [key: string]: string } = {
-  'Dogs': '/images/dog-puppy-on-garden-royalty-free-image-1586966191.avif', // Generic dog image
-  'Cats': '/images/NationalGeographic_2572187_16x9.avif', // Generic cat image
-  'Birds': '/images/bird-category.webp', // Updated to use the new bird image
-  'Fish': '/images/images.webp', // Generic fish image
+  'Dogs': 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*', // Generic dog image
+  'Cats': 'https://m.media-amazon.com/images/I/41i5vzY--rL._AC_UF894,1000_QL80_.jpg', // Generic cat image
+  'Birds': 'https://s7d2.scene7.com/is/image/PetSmart/WEB-APR25W2-AHB6_BRD_DT', // Updated to use the new bird image
+  'Fish': 'https://5.imimg.com/data5/IJ/TD/MY-19422102/fish-aquarium.jpg', // Generic fish image
+  'Small Animals': 'https://images.unsplash.com/photo-1575425170000-21217252121c?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Generic small animal image
 };
