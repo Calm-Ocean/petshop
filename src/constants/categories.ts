f87a108cbacfbde148c5d72d1e530653ed72f14e
[dyad] <madankartik48@gmail.com>
2026-01-04 13:11:57 +0530

    [dyad] wrote 5 file(s), deleted 10 file(s)

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,9 +1,9 @@
 export const ANIMAL_CATEGORIES = ['Dogs', 'Cats', 'Birds', 'Fish', 'Small Animals'];
 
 export const CATEGORY_IMAGES: { [key: string]: string } = {
-  'Dogs': 'src/assets/images/dog-puppy-on-garden-royalty-free-image-1586966191.avif', // Generic dog image
-  'Cats': 'src/assets/images/NationalGeographic_2572187_16x9.avif', // Generic cat image
-  'Birds': '/src/assets/images/bird-category.webp', // Updated to use the new bird image
-  'Fish': 'src/assets/images/images.webp', // Generic fish image
+  'Dogs': '/images/dog-puppy-on-garden-royalty-free-image-1586966191.avif', // Generic dog image
+  'Cats': '/images/NationalGeographic_2572187_16x9.avif', // Generic cat image
+  'Birds': '/images/bird-category.webp', // Updated to use the new bird image
+  'Fish': '/images/images.webp', // Generic fish image
   'Small Animals': 'https://images.unsplash.com/photo-1575425170000-21217252121c?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Generic small animal image
 };
